da907d2cc6790e468036d35d086fb44b97540b9b
Dimetriu <dima.rashidov@gmail.com>
2019-03-01 00:20:52 +0200

    implement create user action

--- app/controllers/api/v1/users/profile_controller.rb
+++ app/controllers/api/v1/users/profile_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::Users::ProfileController < ApiController
   # POST
   def create
+    user = Users::Build.new(user_params)
+
+    if user.call
+      render json: { user: user }, status: :created
+    else
+      render json: { errors: user.errors }, status: :unprocessable_entity
+    end
 
   end
 
@@ -15,4 +22,14 @@ class Api::V1::Users::ProfileController < ApiController
   end
 
   private
+
+  def user_params
+    params.permit(
+      :firstname,
+      :lastname,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
 end
--- app/services/users/build.rb
+++ app/services/users/build.rb
@@ -1,18 +1,15 @@
 # ./services/base_build.rb
 class Users::Build < BaseBuild
-  def initialize(
-    resource = User, **options
-  )
-
+  def initialize(resource = User, **options)
     super(resource, **options)
   end
 
   def call
     resource[:verification_token] = SecureRandom.uuid
+    resource[:session_id] = SecureRandom.uuid
+    # Saving User if valid
     super
-  end
-
-  def self.call(resource = User, **options)
-    super
+    # Logging in User
+    JsonWebToken.encode(session_id: resource.session_id)
   end
 end
