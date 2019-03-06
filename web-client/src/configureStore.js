3e8e0e7c1ee853ad996d8a8c8adc952914cfd18a
Dimetriu <dima.rashidov@gmail.com>
2019-03-06 14:57:46 +0200

    add redux devtools extension

--- web-client/src/configureStore.js
+++ web-client/src/configureStore.js
@@ -6,6 +6,7 @@ import {
 } from 'redux';
 
 import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers/rootReducer';
 import loggerMiddleware from './middleware/logger';
 import monitorReducerEnhancer from './enhancers/monitorReducer';
@@ -15,7 +16,7 @@ export default function configureStore(preloadedState) {
   const middlewareEnhancer = applyMiddleware(...middlewares)
 
   const enhancers = [middlewareEnhancer, monitorReducerEnhancer]
-  const composedEnhancers = compose(...enhancers)
+  const composedEnhancers = composeWithDevTools(...enhancers)
 
   const store = createStore(
     rootReducer,
